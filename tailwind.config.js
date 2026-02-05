a058c357fdd86489fa3eb92dd0fc8e6c6fe10221
Jobelle Gepanago <jobellegepanago12@gmail.com>
2026-02-06 03:22:50 +0800

    Updated file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#257901',
+        backg: '#DEFFCF'
+      }
+    },
+  },
+  plugins: [],
+}
+
