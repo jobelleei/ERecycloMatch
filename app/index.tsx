735526714f73cf7a2d67930e3a31eeed8e613075
Jobelle Gepanago <jobellegepanago12@gmail.com>
2026-02-06 21:03:09 +0800

    Partial Update

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
 import { Text, View, Image, StyleSheet, Pressable } from "react-native";
+import { useRouter } from 'expo-router';
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 justify-center items-center bg-backg">
       
@@ -16,15 +19,20 @@ export default function Index() {
       <Text className="text-1xl mt-3">Recyle Smarter. Match Faster</Text>
 
       <Pressable
-        onPress={() => console.log("Get Started")}
-        className="mt-20 w-64 h-14 bg-primary rounded-full justify-center items-center">
-          <Text className="text-white font-bold">Get Started</Text>
+      onPress={() => router.push('/signup')}
+      className="mt-20 w-64 h-14 bg-primary rounded-full justify-center items-center flex-row">
+        <Text className="text-white font-bold">Get Started</Text>
+        <Image
+          source={require("../assets/images/right-arrow.png")} 
+          style={{ width: 20, height: 25, marginLeft: 10 }}
+          resizeMode="contain"
+        />
       </Pressable>
 
       <Text className="mt-10">OR</Text>
 
       <Pressable
-        onPress={() => console.log("I already have an account")}
+        onPress={() => router.push('/login')}
         className="mt-10 w-64 h-14 bg-white border border-black rounded-full justify-center items-center">
           <Text className="text-black">I already have an account</Text>
       </Pressable>
