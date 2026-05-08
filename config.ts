40421a600a704943fcebee240408ca0346203748
Hana <ejarahannah6@gmail.com>
2026-05-08 13:10:20 +0800

    initial commit

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://172.20.10.3/Admin_Side/admin_UI";
+export const API_URL = "http://10.1.70.231/Admin_Side/admin_UI";
 
-export const YOLO_URL = "http://172.20.10.3:8000";
+export const YOLO_URL = "http://10.1.70.231:8000";
